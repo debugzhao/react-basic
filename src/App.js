ac2f3707587b95ff61cebef9407a47c4225fd592
zhaojingchao <zhaojingchao@joysuch.com>
2024-04-14 19:42:46 +0800

    JSX使用场景

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 
+const name = '赵静超'
+function fun1() {
+  return '王颖'
+}
+
 function App() {
   return (
     <div className="App">
       this is App
+      {/* 使用引号传递字符串 */}
+      {'this is a String'}
+      {/* 识别JS变量 */}
+      {name}
+      {/* 函数调用 */}
+      {fun1()}
+      {/* 方法调用 */}
+      {new Date().getTime()}
+      {/* 使用JS对象 */}
+      <div style={{color:'red'}}>使用JS对象</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+// 导入React必要的两个核心包
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// 导入项目的根组件
 import App from './App';
 
+// 将App组件渲染到id为root的dom节点上
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
