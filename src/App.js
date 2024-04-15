71ac6cf252420126420cfdb67da143c40eb1d5fa
zhaojingchao <zhaojingchao@joysuch.com>
2024-04-15 21:04:12 +0800

    useState的基本使用

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// useState实现一个计数器按钮
+import {useState} from 'react'
 
 const name = '赵静超'
 function fun1() {
@@ -27,11 +29,37 @@ const handleClick2 = (arg, e) => {
   console.log('button被点击了', arg, e)
 }
 
+const Button = () => {
+  return (
+    <button>Click Me!</button>
+  )
+}
+
 function App() {
+  // useState添加一个状态变量，返回的是一个数组
+  // 数组中的第一个参数是状态变量，第二个参数是修改状态变量的方法
+  // useState参数将作为count的初始值
+  const [count, setCount] = useState(0)
+  const [form, setForm] = useState({name: '赵静超'})
+  // 点击事件回调函数
+  const handleClick = ()=> {
+    // 1.用传入的新值修改count
+    // 2.重新使用新的count渲染UI
+    setCount(count + 1);
+  }
+  
+  // 修改对象属性
+  const changeForm = ()=> {
+    setForm({
+      // 通过展开运算符，将form对象中的属性复制到新对象中
+      ...form,
+      name: '王颖'
+    })
+  }
   return (
     <div className="App">
-      this is App
-      <button onClick={(e) => handleClick2('自定义参数', e)}>按钮</button>
+      <button onClick={handleClick}>{count}</button>
+      <button onClick={changeForm}>修改{'  '}{form.name}</button>
     </div>
   );
 }
