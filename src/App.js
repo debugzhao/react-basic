961a5f221fa098e6bcf5d96ea8ae58e38225eece
geek.zhao <jczhao_cs@163.com>
2024-04-16 21:25:59 +0800

    表单受控绑定

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import {useState} from 'react'
 import _, { divide } from 'lodash'
 import classNames from 'classnames'
 
-
-const App = () => {
+// 受控表单绑定
+// 1.通过value属性绑定react状态
+// 2.绑定onChange事件，通过事件参数e拿到输入框最新的值，反向修改react状态
+function App() {
+  const [value, setValue] = useState('')
   return (
-    <div></div>
+    <div>
+      <input value={value}
+        onChange={e => setValue(e.target.value)}
+        type='text'
+      />
+    </div>
   )
 }
 
