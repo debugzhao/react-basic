c6c25fa7ef356a145fae375fb2ba4dbedf29a01b
zhaojingchao <zhaojingchao@joysuch.com>
2024-04-17 17:39:22 +0800

    父子组件通信

--- src/App.js
+++ src/App.js
@@ -1,224 +1,34 @@
 import './App.scss'
 import avatar from './images/bozai.png'
-import {useState, useRef} from 'react'
-import _ from 'lodash'
+import {useState} from 'react'
+import _, { divide } from 'lodash'
 import classNames from 'classnames'
-import {v4 as uuidV4} from 'uuid'
-import dayjs from 'dayjs'
 
-/**
- * 评论列表的渲染和操作
- *
- * 1. 根据状态渲染评论列表
- * 2. 删除评论
- */
-
-// 评论列表数据
-const defaultList = [
-  {
-    // 评论id
-    rpid: 3,
-    // 用户信息
-    user: {
-      uid: '13258165',
-      avatar,
-      uname: '周杰伦',
-    },
-    // 评论内容
-    content: '哎哟，不错哦',
-    // 评论时间
-    ctime: '10-18 08:15',
-    like: 88,
-  },
-  {
-    rpid: 2,
-    user: {
-      uid: '36080105',
-      avatar,
-      uname: '许嵩',
-    },
-    content: '我寻你千百度 日出到迟暮',
-    ctime: '11-13 11:29',
-    like: 88,
-  },
-  {
-    rpid: 1,
-    user: {
-      uid: '30009257',
-      avatar,
-      uname: '黑马前端',
-    },
-    content: '学前端就来黑马',
-    ctime: '10-19 09:00',
-    like: 66,
-  },
-]
-// 当前登录用户信息
-const user = {
-  // 用户id
-  uid: '30009257',
-  // 用户头像
-  avatar,
-  // 用户昵称
-  uname: '黑马前端',
-}
-
-/**
- * 导航 Tab 的渲染和操作
- *
- * 1. 渲染导航 Tab 和高亮
- * 2. 评论列表排序
- *  最热 => 喜欢数量降序
- *  最新 => 创建时间降序
- */
-
-// 导航 Tab 数组
-const tabs = [
-  { type: 'hot', text: '最热' },
-  { type: 'time', text: '最新' },
-]
-
-
-const App = () => {
-  // 渲染评论列表
-  // 使用useState维护list
-  const [commentList, setCommentList] = useState(_.orderBy(defaultList, 'like', 'desc'))
-  const [type, setType] = useState('hot')
-  // 用户新增的评论
-  const [content, setContent] = useState('')
-  // 评论框dom ref
-  const inputRef = useRef(null)
-  const handlerDel = (uid)=> {
-    console.log('uid', uid)
-    setCommentList(commentList.filter(item => item.user.uid !== uid))
-  }
-  
-  const handlePublish = ()=> {
-    setCommentList([
-      // 展开运算符
-      ...commentList,
-      {
-        // 评论id
-        rpid: uuidV4(),
-        // 用户信息
-        user: {
-          uid: '13258165',
-          avatar,
-          uname: '周杰伦',
-        },
-        // 评论内容
-        content: content,
-        // 评论时间
-        ctime: dayjs(new Date()).format('MM-DD hh:mm'), // 格式化为 月-日 时:分
-        like: 188,
-      },
-    ])
-    // 1.清空评论框内容
-    setContent('')
-    // 2.重新聚焦评论框
-    inputRef.current.focus()
-  }
-
-  const handleTabChange = (type)=> {
-    console.log('type', type)
-    setType(type)
-    if (type === 'hot') {
-      // 根据点赞数排序
-      setCommentList(_.orderBy(commentList, 'like', 'desc'))
-    } else {
-      // 根据热度排序
-      setCommentList(_.orderBy(commentList, 'ctime', 'desc'))
-    }
-  }
-
+// 父子组件之间通信
+// 1.父组件传递数据
+// 2.子组件通过props接受数据
+const Son = (props)=> {
+  console.log(props)
+  // sonCb箭头函数接收父组件传递过来的回调函数
+  const sonCb = () => props.cb()
+  sonCb()
   return (
-    <div className="app">
-      {/* 导航 Tab */}
-      <div className="reply-navigation">
-        <ul className="nav-bar">
-          <li className="nav-title">
-            <span className="nav-title-text">评论</span>
-            {/* 评论数量 */}
-            <span className="total-reply">{10}</span>
-          </li>
-          <li className="nav-sort">
-            {/* 高亮类名： active */}
-            {tabs.map(item => (
-              <span 
-                key={item.type} 
-                onClick={() => handleTabChange(item.type)} 
-                className={classNames('nav-item', {active: type === item.type})}>{item.text}
-              </span>
-            ))}
-          </li>
-        </ul>
-      </div>
-
-      <div className="reply-wrap">
-        {/* 发表评论 */}
-        <div className="box-normal">
-          {/* 当前用户头像 */}
-          <div className="reply-box-avatar">
-            <div className="bili-avatar">
-              <img className="bili-avatar-img" src={avatar} alt="用户头像" />
-            </div>
-          </div>
-          <div className="reply-box-wrap">
-            {/* 评论框 */}
-            <textarea
-              className="reply-box-textarea"
-              placeholder="发一条友善的评论"
-              value={content}
-              ref={inputRef}
-              onChange={(e) => setContent(e.target.value)}
-            />
-            {/* 发布按钮 */}
-            <div className="reply-box-send">
-              <div className="send-text" onClick={handlePublish}>发布</div>
-            </div>
-          </div>
-        </div>
-        {/* 评论列表 */}
-        <div className="reply-list">
-          {/* 评论项 */}
-          {commentList.map(item => (
-            <div key={item.rpid} className="reply-item">
-              {/* 头像 */}
-              <div className="root-reply-avatar">
-                <div className="bili-avatar">
-                  <img
-                    className="bili-avatar-img"
-                    alt=""
-                    src={item.user.avatar}
-                  />
-                </div>
-              </div>
-
-              <div className="content-wrap">
-                {/* 用户名 */}
-                <div className="user-info">
-                  <div className="user-name">{item.user.uname}</div>
-                </div>
-                {/* 评论内容 */}
-                <div className="root-reply">
-                  <span className="reply-content">{item.content}</span>
-                  <div className="reply-info">
-                    {/* 评论时间 */}
-                    <span className="reply-time">{item.ctime}</span>
-                    {/* 评论数量 */}
-                    <span className="reply-time">点赞数:{item.like}</span>
-                    {item.user.uid === user.uid &&             
-                      <span className="delete-btn" onClick={() => handlerDel(item.user.uid)}>
-                        删除
-                      </span>
-                    }
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+    <div>children data, {props.name}, {props.jsx}</div>
+  )
+}
+function App() {
+  const data = 'this is parent component data'
+  return (
+    <div>
+      <Son 
+        name={data}
+        age={18}
+        isTrue={false}
+        list={['React', 'Vue', 'Angular']}
+        obj={{nickname: 'zjc'}}
+        cb={() => {console.log('this is a callback function')}}
+        jsx={<span>this is span</span>}
+      />
     </div>
   )
 }
